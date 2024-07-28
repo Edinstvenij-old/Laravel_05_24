ecb4e6356a03f858d5550543b6794cd0b33d0cee
nikvi <nikvigovskij@gmail.com>
2024-07-28 15:50:37 +0300

    AJAX, JS for images on the admin products pages

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -4,19 +4,16 @@
     <div class="container">
         <div class="row">
             <div class="col-12 d-flex align-items-center justify-content-center pt-5">
-                <form class="card w-50" method="POST" enctype="multipart/form-data" action="{{route('admin.products.store')}}">
+                <form class="card w-50" method="POST" enctype="multipart/form-data" action="{{ route('admin.products.update', $product) }}">
                     @csrf
+                    @method('PUT')
 
                     <h5 class="card-header">Edit product</h5>
                     <div class="card-body">
                         <div class="row mb-3">
                             <label for="title" class="col-md-4 col-form-label text-md-end">{{ __('Title') }}</label>
-
                             <div class="col-md-6">
-                                <input id="title" type="text"
-                                       class="form-control @error('title') is-invalid @enderror" name="title"
-                                       value="{{ old('title') ?? $product->title }}" required autofocus>
-
+                                <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') ?? $product->title }}" required autofocus>
                                 @error('title')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -25,14 +22,11 @@
                             </div>
                         </div>
 
+                        <!-- SKU field -->
                         <div class="row mb-3">
                             <label for="SKU" class="col-md-4 col-form-label text-md-end">{{ __('SKU') }}</label>
-
                             <div class="col-md-6">
-                                <input id="SKU" type="text"
-                                       class="form-control @error('SKU') is-invalid @enderror" name="SKU"
-                                       value="{{ old('SKU') ?? $product->SKU }}" required>
-
+                                <input id="SKU" type="text" class="form-control @error('SKU') is-invalid @enderror" name="SKU" value="{{ old('SKU') ?? $product->SKU }}" required>
                                 @error('SKU')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -41,31 +35,28 @@
                             </div>
                         </div>
 
+                        <!-- Description field -->
                         <div class="row mb-3">
-                            <label for="description"
-                                   class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
-
+                            <label for="description" class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
                             <div class="col-md-6">
-                                    <textarea id="description" type="text"
-                                              class="form-control" name="description"
-                                    >{{ old('description') ?? $product->description }}</textarea>
+                                <textarea id="description" class="form-control @error('description') is-invalid @enderror" name="description">{{ old('description') ?? $product->description }}</textarea>
+                                @error('description')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
 
+                        <!-- Categories field -->
                         <div class="row mb-3">
-                            <label for="categories"
-                                   class="col-md-4 col-form-label text-md-end">{{ __('Categories') }}</label>
-
+                            <label for="categories" class="col-md-4 col-form-label text-md-end">{{ __('Categories') }}</label>
                             <div class="col-md-6">
-                                <select name="categories[]" id="categories"
-                                        class="form-control @error('categories') is-invalid @enderror" multiple>
+                                <select name="categories[]" id="categories" class="form-control @error('categories') is-invalid @enderror" multiple>
                                     @foreach($categories as $category)
-                                        <option value="{{$category->id}}"
-                                                @if (in_array($category->id, $productCategories)) selected @endif
-                                        >{{$category->name}}</option>
+                                        <option value="{{ $category->id }}" @if(in_array($category->id, $productCategories)) selected @endif>{{ $category->name }}</option>
                                     @endforeach
                                 </select>
-
                                 @error('categories')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -74,16 +65,11 @@
                             </div>
                         </div>
 
+                        <!-- Price field -->
                         <div class="row mb-3">
                             <label for="price" class="col-md-4 col-form-label text-md-end">{{ __('Price') }}</label>
-
                             <div class="col-md-6">
-                                <input id="price" type="number"
-                                       class="form-control @error('price') is-invalid @enderror" name="price"
-                                       value="{{ old('price') ?? $product->price }}"
-                                       step="any"
-                                       required>
-
+                                <input id="price" type="number" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ old('price') ?? $product->price }}" step="any" required>
                                 @error('price')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -92,19 +78,11 @@
                             </div>
                         </div>
 
+                        <!-- Discount field -->
                         <div class="row mb-3">
-                            <label for="discount"
-                                   class="col-md-4 col-form-label text-md-end">{{ __('Discount') }}</label>
-
+                            <label for="discount" class="col-md-4 col-form-label text-md-end">{{ __('Discount') }}</label>
                             <div class="col-md-6">
-                                <input id="discount" type="number"
-                                       class="form-control @error('discount') is-invalid @enderror" name="discount"
-                                       value="{{ old('discount') ?? $product->discount }}"
-                                       step="any"
-                                       min="0"
-                                       max="99"
-                                       required>
-
+                                <input id="discount" type="number" class="form-control @error('discount') is-invalid @enderror" name="discount" value="{{ old('discount') ?? $product->discount }}" step="any" min="0" max="99" required>
                                 @error('discount')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -113,15 +91,11 @@
                             </div>
                         </div>
 
+                        <!-- Quantity field -->
                         <div class="row mb-3">
-                            <label for="quantity"
-                                   class="col-md-4 col-form-label text-md-end">{{ __('Quantity') }}</label>
-
+                            <label for="quantity" class="col-md-4 col-form-label text-md-end">{{ __('Quantity') }}</label>
                             <div class="col-md-6">
-                                <input id="quantity" type="number"
-                                       class="form-control @error('quantity') is-invalid @enderror" name="quantity"
-                                       value="{{ old('quantity') ?? $product->quantity }}">
-
+                                <input id="quantity" type="number" class="form-control @error('quantity') is-invalid @enderror" name="quantity" value="{{ old('quantity') ?? $product->quantity }}">
                                 @error('quantity')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -130,17 +104,14 @@
                             </div>
                         </div>
 
+                        <!-- Thumbnail field -->
                         <div class="row mb-3">
-                            <label for="thumbnail"
-                                   class="col-md-4 col-form-label text-md-end">{{ __('Thumbnail') }}</label>
-
+                            <label for="thumbnail" class="col-md-4 col-form-label text-md-end">{{ __('Thumbnail') }}</label>
                             <div class="col-md-12 mb-4 d-flex align-items-center justify-content-center">
-                                <img src="{{$product->thumbnailUrl}}" id="thumbnail-preview" style="width: 50%;" />
+                                <img src="{{ $product->thumbnailUrl }}" id="thumbnail-preview" style="width: 50%;" />
                             </div>
                             <div class="col-md-12">
-                                <input id="thumbnail" type="file"
-                                       class="form-control @error('thumbnail') is-invalid @enderror" name="thumbnail">
-
+                                <input id="thumbnail" type="file" class="form-control @error('thumbnail') is-invalid @enderror" name="thumbnail">
                                 @error('thumbnail')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -149,19 +120,17 @@
                             </div>
                         </div>
 
+                        <!-- Additional Images field -->
                         <div class="row mb-3">
-                            <label for="images"
-                                   class="col-md-4 col-form-label text-md-end">{{ __('Additional Images') }}</label>
-
+                            <label for="images" class="col-md-4 col-form-label text-md-end">{{ __('Additional Images') }}</label>
                             <div class="col-12 mb-4 d-flex align-items-center justify-content-center">
                                 <div id="images-wrapper" class="row">
                                     @foreach($product->images as $image)
                                         <div class='mb-4 col-md-6 images-wrapper-item'>
-                                            <button class="btn btn-danger images-wrapper-item-remove"
-                                                    data-url="{{route('ajax.image.remove', $image)}}">
+                                            <button class="btn btn-danger images-wrapper-item-remove" data-url="{{ route('ajax.image.remove', $image) }}">
                                                 <i class="fa-solid fa-minus"></i>
                                             </button>
-                                            <img src='{{$image->url}}' style='width: 100%' />
+                                            <img src='{{ $image->url }}' style='width: 100%' />
                                         </div>
                                     @endforeach
                                 </div>
@@ -169,24 +138,23 @@
                             <div class="col-md-12">
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <input id="images" type="file"
-                                               class="form-control @error('images') is-invalid @enderror d-none" name="images[]" multiple>
+                                        <input id="images" type="file" class="form-control @error('images') is-invalid @enderror d-none" name="images[]" multiple>
                                     </div>
                                     <div class="col-md-6 d-flex align-items-center justify-content-end">
                                         <div id="spinner" class="spinner-border d-none me-2" role="status">
                                             <span class="visually-hidden">Loading...</span>
                                         </div>
-                                        <button class="btn btn-outline-success add-images"
-                                                data-upload="{{route('ajax.product.images.upload', $product)}}"
-                                        >Upload images <i class="fa-solid fa-plus"></i></button>
+                                        <button class="btn btn-outline-success add-images" data-upload="{{ route('ajax.product.images.upload', $product) }}">
+                                            Upload images <i class="fa-solid fa-plus"></i>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
                     </div>
+
                     <div class="card-footer d-flex align-items-center justify-content-end">
-                        <button type="submit" class="btn btn-outline-success">Create</button>
+                        <button type="submit" class="btn btn-outline-success">Edit</button>
                     </div>
                 </form>
             </div>
